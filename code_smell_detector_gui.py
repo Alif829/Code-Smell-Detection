547f2c9965a4cc4bf510b5f1f7fe37a2a7a9c3aa
Alif829 <bsse1120@iit.du.ac.bd>
2023-12-12 06:18:47 +0600

    gui cleaned

--- code_smell_detector_gui.py
+++ code_smell_detector_gui.py
@@ -1,8 +1,8 @@
+import customtkinter as ctk
 import tkinter as tk
 from generate_ast import generate_ast
-from tkinter import scrolledtext, Spinbox, Label
-from tkinter import messagebox
-from python_smell_detector import detect_code_smells
+from customtkinter import *
+from PIL import Image
 
 from Detectors.long_parameter_list import detect_long_parameter_list
 from Detectors.large_method import detect_long_method
@@ -13,13 +13,10 @@ from Detectors.complex_lambda_function import detect_complex_lambda
 
 def detect_smells():
     code = code_input.get("1.0", tk.END)
-
-    # Tree-sitter ast
     tree = generate_ast(code)
     root_node = tree.root_node
 
     try:
-        # Retrieving threshold values
         param_thresh = int(parameter_threshold.get())
         return_thresh = int(return_threshold.get())
         max_nesting_level_val = int(max_nesting_level.get())
@@ -27,89 +24,138 @@ def detect_smells():
         lambda_complexity = int(complexity.get())
         child_thresh = int(child_threshold.get())
 
-        # Pass these values to the detection function
         smells = []
-
         for node in root_node.children:
-
             if node.type == 'function_definition':
-                # Duplicated Code Smell
                 duplicate_code_issues = detect_duplicate_code(node, code)
                 for issue in duplicate_code_issues:
                     smells.append((issue, 'Duplicate Code'))
-                    
-                
-                # Long Parameter List Smell
-                if detect_long_parameter_list(node, param_thresh):
-                    smells.extend([(smell_node, 'Long Parameter List') for smell_node in detect_long_parameter_list(node,parameter_threshold)])
-                    
-                
-                # Excessive Return Smell
-                excessive_returns = detect_excessive_returns(node, return_thresh)
-                if excessive_returns:
-                    for return_node in excessive_returns:
-                        smells.append((return_node, 'Excessive Returns'))
-                        
 
-                # Complex Method Smell
+                long_parameter_list_issues = detect_long_parameter_list(node, param_thresh)
+                for issue in long_parameter_list_issues:
+                    smells.append((issue, 'Long Parameter List'))
+
+                excessive_returns_issues = detect_excessive_returns(node, return_thresh)
+                for issue in excessive_returns_issues:
+                    smells.append((issue, 'Excessive Returns'))
+
                 complex_method_issues = detect_complex_method(node, max_nesting_level_val, max_statement_count_val)
                 for issue in complex_method_issues:
                     smells.append((issue, 'Complex Method'))
-                    
-                
-                # Complex Lambda Functions
+
                 complex_lambda_issues = detect_complex_lambda(node, lambda_complexity)
                 for issue in complex_lambda_issues:
                     smells.append((issue, 'Complex Lambda'))
-                    
-                
-                # Large method smell
-                large_method_issues = detect_long_method(node,code, child_thresh)
+
+                large_method_issues = detect_long_method(node, code, child_thresh)
                 for issue in large_method_issues:
                     smells.append((issue, 'Large Method'))
-
 
         result_output.delete("1.0", tk.END)
         result_output.insert(tk.INSERT, smells)
     except Exception as e:
-        messagebox.showerror("Error", str(e))
+        ctk.messagebox.showerror("Error", str(e))
+
 
 # Set up the main application window
-root = tk.Tk()
+ctk.set_appearance_mode("Dark")  # Set dark theme
+root = ctk.CTk()
 root.title("Code Smell Detector")
 
-# Code input area
-code_input = scrolledtext.ScrolledText(root, height=15)
+root.grid_rowconfigure(0, weight=1)
+root.grid_columnconfigure(1, weight=1)
+
+# Create frames
+left_frame = ctk.CTkFrame(root)
+middle_frame = ctk.CTkFrame(root)
+right_frame = ctk.CTkFrame(root)
+
+left_frame.grid(row=0, column=0, padx=10, pady=10, sticky="ns")
+middle_frame.grid(row=0, column=1, padx=10, pady=10, sticky="nsew")
+right_frame.grid(row=0, column=2, padx=10, pady=10, sticky="ns")
+
+left_frame.grid_columnconfigure(0, weight=1)
+middle_frame.grid_rowconfigure(0, weight=1)
+middle_frame.grid_columnconfigure(0, weight=1)
+right_frame.grid_columnconfigure(0, weight=1)
+
+# Left Frame - List of code smells
+logo_img_data = Image.open("Icons/logo.png")
+logo_img = CTkImage(dark_image=logo_img_data, light_image=logo_img_data, size=(77.68, 85.42))
+
+CTkLabel(master=left_frame, text="", image=logo_img).pack(pady=(38, 0), anchor="center")
+CTkLabel(master=left_frame, text="Code Smells",fg_color="transparent", font=("Arial Bold", 18)).pack(pady=10,padx=10, anchor="center")
+
+analytics_img_data = Image.open("Icons/duplicate.png")
+analytics_img = CTkImage(dark_image=analytics_img_data, light_image=analytics_img_data)
+CTkButton(master=left_frame,width=200 ,image=analytics_img, text="Code Duplication", fg_color="transparent", font=("Arial Bold", 14), hover_color="#207244", anchor="w").pack(anchor="center", ipady=5, pady=(60, 0))
+
+package_img_data = Image.open("Icons/lambda.png")
+package_img = CTkImage(dark_image=package_img_data, light_image=package_img_data)
+CTkButton(master=left_frame,width=200, image=package_img, text="Complex Lambda", fg_color="transparent", font=("Arial Bold", 14), hover_color="#207244", anchor="w").pack(anchor="center", ipady=5, pady=(16, 0))
+
+list_img_data = Image.open("Icons/complex.png")
+list_img = CTkImage(dark_image=list_img_data, light_image=list_img_data)
+CTkButton(master=left_frame,width=200, image=list_img, text="Complex Method", fg_color="transparent", font=("Arial Bold", 14), hover_color="#207244", anchor="w").pack(anchor="center", ipady=5, pady=(16, 0))
+
+returns_img_data = Image.open("Icons/large.png")
+returns_img = CTkImage(dark_image=returns_img_data, light_image=returns_img_data)
+CTkButton(master=left_frame,width=200, image=returns_img, text="Large Method", fg_color="transparent", font=("Arial Bold", 14), hover_color="#207244", anchor="w").pack(anchor="center", ipady=5, pady=(16, 0))
+
+settings_img_data = Image.open("Icons/returns_icon.png")
+settings_img = CTkImage(dark_image=settings_img_data, light_image=settings_img_data)
+CTkButton(master=left_frame,width=200, image=settings_img, text="Excessive Returns", fg_color="transparent", font=("Arial Bold", 14), hover_color="#207244", anchor="w").pack(anchor="center", ipady=5, pady=(16, 0))
+
+person_img_data = Image.open("Icons/parameter.png")
+person_img = CTkImage(dark_image=person_img_data, light_image=person_img_data)
+CTkButton(master=left_frame,width=200, image=person_img, text="Long Parameter List", fg_color="transparent", font=("Arial Bold", 14), hover_color="#207244", anchor="w").pack(anchor="center", ipady=5, pady=(16, 0))
+
+
+# Middle Frame - Code input and output
+code_input = ctk.CTkTextbox(middle_frame, height=300,width=900,scrollbar_button_color="#FFCC70",corner_radius=16)
 code_input.pack(pady=10)
-
-# Threshold inputs
-parameter_threshold = Spinbox(root, from_=1, to=10, width=5)
-return_threshold = Spinbox(root, from_=1, to=10, width=5)
-max_nesting_level = Spinbox(root, from_=1, to=10, width=5)
-max_statement_count = Spinbox(root, from_=1, to=50, width=5)
-complexity = Spinbox(root, from_=1, to=10, width=5)
-child_threshold = Spinbox(root, from_=1, to=50, width=5)
-
-# Labels for threshold inputs
-Label(root, text="Parameter Threshold:").pack()
-parameter_threshold.pack()
-Label(root, text="Return Threshold:").pack()
-return_threshold.pack()
-Label(root, text="Max Nesting Level:").pack()
-max_nesting_level.pack()
-Label(root, text="Max Statement Count:").pack()
-max_statement_count.pack()
-Label(root, text="Lambda Complexity:").pack()
-complexity.pack()
-Label(root, text="Child Threshold:").pack()
-child_threshold.pack()
-
-# Detect button
-detect_button = tk.Button(root, text="Detect Smells", command=detect_smells)
-detect_button.pack()
-
-# Result output area
-result_output = scrolledtext.ScrolledText(root, height=15)
+result_output = ctk.CTkTextbox(middle_frame, height=200,width=900,scrollbar_button_color="#FFCC70",corner_radius=16)
 result_output.pack(pady=10)
+
+# Right Frame - Threshold inputs
+parameter_threshold = ctk.CTkEntry(right_frame, width=200)
+return_threshold = ctk.CTkEntry(right_frame, width=200)
+max_nesting_level = ctk.CTkEntry(right_frame, width=200)
+max_statement_count = ctk.CTkEntry(right_frame, width=200)
+complexity = ctk.CTkEntry(right_frame, width=200)
+child_threshold = ctk.CTkEntry(right_frame, width=200)
+
+# Place threshold entries with labels
+ctk.CTkLabel(right_frame, text="Parameter Threshold:").pack()
+parameter_threshold.pack(pady=2)
+ctk.CTkLabel(right_frame, text="Return Threshold:").pack()
+return_threshold.pack(pady=2)
+ctk.CTkLabel(right_frame, text="Max Nesting Level:").pack()
+max_nesting_level.pack(pady=2)
+ctk.CTkLabel(right_frame, text="Max Statement Count:").pack()
+max_statement_count.pack(pady=2)
+ctk.CTkLabel(right_frame, text="Lambda Complexity:").pack()
+complexity.pack(pady=2)
+ctk.CTkLabel(right_frame, text="Child Threshold:").pack()
+child_threshold.pack(pady=2)
+
+# Create and configure the Detect button
+detect_button = ctk.CTkButton(middle_frame, text="Detect Smells", fg_color="#207244", command=detect_smells)
+detect_button.pack(side="left", padx=90)  # Place it to the left
+
+
+# Create a function to handle file selection and input
+def open_file():
+    file_path = filedialog.askopenfilename(filetypes=[("Python files", "*.py")])
+    if file_path:
+        with open(file_path, 'r') as file:
+            code_input.delete("1.0", tk.END)
+            code_input.insert(tk.END, file.read())
+
+# Create a "Open File" button to trigger the file dialog
+open_file_button = ctk.CTkButton(middle_frame, text="Open File", fg_color="#207244", command=open_file)
+open_file_button.pack(side="right", padx=100)  # Place it to the left
+
+
 
 root.mainloop()
